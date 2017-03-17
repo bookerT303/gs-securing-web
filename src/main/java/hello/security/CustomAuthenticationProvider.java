28e77c2c9f37509554617b97f9d8b316a40f82a3
Brad Davidson <bdavidson@pivotal.io>
2017-03-17 14:23:51 -0400

    Change authentication provider to work for same hard coded users.

--- src/main/java/hello/security/CustomAuthenticationProvider.java
+++ src/main/java/hello/security/CustomAuthenticationProvider.java
@@ -8,12 +8,25 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 
 @Component
 public class CustomAuthenticationProvider
         implements AuthenticationProvider {
+    private final List<UserData> users;
+
+    public CustomAuthenticationProvider() {
+        users = asList(
+                new UserData("user", "password", true, asList(new Role("ROLE_USER"))),
+                new UserData("admin", "admin", true, asList(
+                        new Role("ROLE_USER"),
+                        new Role("ROLE_ADMIN")))
+        );
+    }
 
     @Override
     public Authentication authenticate(Authentication authentication)
@@ -22,13 +35,19 @@ public class CustomAuthenticationProvider
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
 
-        if ("NO-WAY".equalsIgnoreCase(name)) {
-            throw new UsernameNotFoundException("Invalid User");
-        }
-        System.out.printf("User: %s, Password: %s", name, password);
-        return new UsernamePasswordAuthenticationToken(
-                name, password, asList(new GrantedAuthorityImpl("ROLE_USER")
-        ));
+        // WARNING... UserDetailsService returns the expected password and Spring will
+        // validate that the user did login with the correct password....
+        // HOWEVER the authenticationProvider is expected to have validated the password and the returned
+        // password is really just informational...
+        return users.stream()
+                .filter(entry -> entry.getUsername().equals(name) && entry.getPassword().equals(password))
+                .findFirst()
+                .map(userData -> new UsernamePasswordAuthenticationToken(
+                    userData.getUsername(), userData.getPassword(),
+                    userData.getRoles().stream()
+                        .map(role -> new GrantedAuthorityImpl(role.getRole()))
+                        .collect(Collectors.toList())))
+                .orElse(null);
     }
 
     @Override
